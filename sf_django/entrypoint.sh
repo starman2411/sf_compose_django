bf45e4fb6d011371156a940995936d85a407f2c2
starman2411 <61166642+starman2411@users.noreply.github.com>
2022-12-18 01:44:57 +0300

    Add files via upload

---
+++ sf_django/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+echo "started!"
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py flush --no-input
+python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'admin')" | python manage.py shell
+#python manage.py runserver
+
+gunicorn sf_django.wsgi:application --bind 0.0.0.0:8000
+
+exec "$@"
+
